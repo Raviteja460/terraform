9bf7f75dc654e98fedb2a9130b5ad3c6bb5a5ac5
Raviteja460 <seelamsettyraviteja@gmail.com>
2024-01-08 10:10:54 +0530

    Terraform

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -1,68 +1,36 @@
-# resource "aws_instance" "web" {
-#   ami           = "ami-03265a0778a880afb" #devops-practice
-#   instance_type = "t2.micro"
-#   vpc_security_group_ids = [aws_security_group.sg.id] # this means list
-
-#   tags = {
-#     Name = "HelloTerraform"
-#   }
-# }
-
-# resource "aws_security_group" "sg" {
-#   name        = var.sg-name # this is for AWS
-#   description = var.sg-description
-#     #vpc_id      = aws_vpc.main.id
-
-#   ingress {
-#     description      = "Allow All ports"
-#     from_port        = var.inbound-from-port # 0 means all ports
-#     to_port          = 0 
-#     protocol         = "tcp"
-#     cidr_blocks      = var.cidr_blocks
-#         #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-#   }
-
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#         #ipv6_cidr_blocks = ["::/0"]
-#   }
-#   tags = {
-#     Name = var.sg-name
-#   }
-# }
-
-resource "aws_instance" "web" {  #Sampleinstance is terraform name
-  ami           = "ami-03265a0778a880afb" #devops-practice ami
+resource "aws_instance" "web" {
+  ami           = "ami-03265a0778a880afb" #devops-practice
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.SampleSG.id]
+  vpc_security_group_ids = [aws_security_group.sg.id] # this means list
+
   tags = {
-    Name = "TerraformEc2" #Ec2 Instance name
+    Name = "HelloTerraform"
   }
 }
-resource "aws_security_group" "SampleSG" {
-  name        = var.sg-name
+
+resource "aws_security_group" "sg" {
+  name        = var.sg-name # this is for AWS
   description = var.sg-description
-  #vpc_id      = aws_vpc.main.id
+    #vpc_id      = aws_vpc.main.id
+
   ingress {
-    description      = "TLS from VPC"
-    from_port        = var.from-port
-    to_port          = 0
+    description      = "Allow All ports"
+    from_port        = var.inbound-from-port # 0 means all ports
+    to_port          = 0 
     protocol         = "tcp"
-    cidr_blocks      = var.cidr_range
-    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = var.cidr_blocks
+        #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
+
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    #ipv6_cidr_blocks = ["::/0"]
+        #ipv6_cidr_blocks = ["::/0"]
   }
   tags = {
-    Name = "TerraformSG"
+    Name = var.sg-name
   }
 }
 
--- session-01/ec2/variables.tf
+++ session-01/ec2/variables.tf
@@ -1,38 +1,19 @@
-# variable "sg-name" {
-#   type = string
-#   default = "roboshop-all-aws"
-# }
-
-# variable "sg-description" {
-#   type = string
-#   default = "Allow TLS inbound traffic"
-# }
-
-# variable "inbound-from-port" {
-#   #type = string
-#   default = 0
-# }
-
-# variable "cidr_blocks" {
-#   type = list
-#   default = ["0.0.0.0/0"]
-# }
-
 variable "sg-name" {
-    type = string   # No need to give type=string,number etc. It's for user understanding.
-    default = "TerraformSG"
-}
-variable "sg-description" {
-    type = string
-    default = "Allow TLS inbound traffic"
-}
-variable "from-port" {
-    type = number
-    default = 0
-}
-variable "cidr_range" {
-    type = list
-    default = ["0.0.0.0/0"]
+  type = string
+  default = "roboshop-all-aws"
 }
 
- 
+variable "sg-description" {
+  type = string
+  default = "Allow TLS inbound traffic"
+}
+
+variable "inbound-from-port" {
+  #type = string
+  default = 0
+}
+
+variable "cidr_blocks" {
+  type = list
+  default = ["0.0.0.0/0"]
+}
